8abacb8a8587f9e2a997130bd8b55a6b55dadbfe
Jan-Philip Loos <maxdaten@gmail.com>
2013-10-17 07:13:29 +0200

    fixed primitives and partially normal calculation

--- src/Yage/Math.hs
+++ src/Yage/Math.hs
@@ -1,7 +1,22 @@
 module Yage.Math
-    (fromTransformation) where
+    (fromTransformation
+    , (><)
+    , normal, normals
+    , plainNormalForm) where
 
-import Linear (V3(..), V4(..), M33, M44)
+import Yage.Prelude
+import Control.Lens
+import Data.List ((++), map)
+import Linear (V3(..), V4(..), M33, M44, cross, normalize, Epsilon)
+
+type Normal a = V3 a
+-- | a plain in 3d space in plain normal form 
+type Plain3DNF a = (Normal a, V3 a, V3 a)
+
+infixl 7 ><
+
+(><):: Num a => V3 a -> V3 a -> V3 a
+(><) = cross
 
 fromTransformation :: M44 a -> M33 a
 fromTransformation
@@ -11,3 +26,30 @@ fromTransformation
         (V4 _ _ _ _)) = V3 (V3 a b c)
                            (V3 d e f)
                            (V3 g h i)
+
+-- | calculates a normal from two vectors in local space (no position vectors)
+normal :: (Num a, Floating a, Epsilon a) => V3 a -> V3 a -> V3 a
+normal v1 v2 = normalize $ cross v1 v2
+
+
+-- | calculates a plain in normal form from three position vectors
+plainNormalForm :: (Num a, Floating a, Epsilon a) => V3 a -> V3 a -> V3 a -> Plain3DNF a
+plainNormalForm v1 v2 v3 = 
+    let n = normal (v1 - v2) (v2 - v3)
+        p = v1
+        q = v2
+    in (n, p, q)
+
+-- possible not the fastest implementation
+normals :: (Num a, Floating a, Epsilon a) => [V3 a] -> [V3 a]
+normals vs = map norms $ splitEvery 3 vs
+    where
+        norms (a:b:c:[]) = (plainNormalForm a b c)^._1
+
+{--
+genNormals :: (Num a, Show a, Floating a) => [V3 a] -> [V3 a]
+genNormals vs =
+    let ns = concat $ map (\(a:b:_) -> replicate 3 $ a `normal` b) $ splitEvery 3 vs
+        cs = repeat color
+    in zipWith3 Vertex vs ns cs
+--}
--- src/Yage/Rendering/Primitives.hs
+++ src/Yage/Rendering/Primitives.hs
@@ -1,65 +1,86 @@
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Yage.Rendering.Primitives where
 
-import Yage.Prelude hiding (id)
-import Control.Lens ((^.))
+import Yage.Prelude -- hiding (id)
+import Control.Lens hiding (Index)
+-- import Control.Lens -- ((^.))
 
 import Data.List ((++), reverse, map, take, length, (!!), concat, replicate, repeat, zipWith3)
-import Linear (point)
+
+import Linear (V2(..), V3(..), V4(..), R3(_xyz), point)
 
 import Yage.Rendering.Types
 import Yage.Rendering.VertexSpec
+import Yage.Math
 
-import Linear (V3(..), V4(..), R3(_xyz), cross, signorm)
+--one, zero :: CFloat
+one  = 1.0
+zero = 0.0
+-- f = front; h = hidden; t = top; b = bottom; r = right; l = left
+f     =  one
+h     = -one
+t     =  one
+b     = -one
+r     =  one
+l     = -one
 
+white = (V4 1.0 1.0 1.0 1.0)
 
 cubeMesh :: Mesh Vertex434
 cubeMesh = 
-    let verts = point 
-                <$> [ V3 (-0.5) 0.5 0.5   , V3 0.5 0.5 0.5   , V3 0.5 (-0.5) 0.5   , V3 (-0.5) (-0.5) 0.5
-                    , V3 (-0.5) 0.5 (-0.5), V3 0.5 0.5 (-0.5), V3 0.5 (-0.5) (-0.5), V3 (-0.5) (-0.5) (-0.5)
+    let 
+        ltf   = V3 l t f
+        rtf   = V3 r t f
+        rbf   = V3 r b f
+        lbf   = V3 l b f
+        lth   = V3 l t h
+        rth   = V3 r t h
+        rbh   = V3 r b h
+        lbh   = V3 l b h
+        verts = point 
+                <$> [ ltf, lbf, rbf, rtf
+                    , lth, rth, rbh, lbh
                     ]
-        frontFace = [0, 3, 2, 0, 2, 1] :: [Int]
-        leftFace  = [0, 4, 7, 0, 7, 3] :: [Int]
-        rightFace = [1, 2, 6, 1, 6, 5] :: [Int]
-        topFace   = [0, 1, 4, 4, 1, 5] :: [Int]
-        bottomFace= [7, 2, 3, 7, 6, 2] :: [Int]
+        frontFace = [0, 1, 2, 2, 3, 0] :: [Int]
+        leftFace  = [0, 4, 7, 7, 1, 0] :: [Int]
+        rightFace = [2, 6, 5, 5, 3, 2] :: [Int]
+        topFace   = [0, 3, 5, 5, 4, 0] :: [Int]
+        bottomFace= [1, 2, 6, 6, 7, 1] :: [Int]
         ixs       = frontFace
                   ++ leftFace
                   ++ topFace
                   ++ reverse (map (+4) frontFace) -- back
                   ++ rightFace
                   ++ bottomFace
-    in mkTriMeshfromSpare "cube" (traceShow' verts) (traceShow' ixs) (V4 1.0 1.0 1.0 1.0)
+    in makeMeshfromSpare "cube" (traceShow' verts) (traceShow' ixs) white
 
 quadMesh :: Mesh Vertex434
 quadMesh = 
-    let verts = point 
-                <$> [ V3 (-0.5) 0.5 0.0, V3 (-0.5) (-0.5) 0.0, V3 0.5 (-0.5) 0.0, V3 0.5 0.5 0.0 ]
-        ixs   = [0, 1, 2, 0, 2, 3]
-        color = V4 1.0 1.0 1.0 1.0
-    in mkTriMeshfromSpare "quad" verts ixs color
+    let tl    = V3 (-one)   one  zero
+        bl    = V3 (-one) (-one) zero
+        br    = V3   one  (-one) zero
+        tr    = V3   one    one  zero
+        verts = point <$> [tl, bl, br, tr]
+        ixs   = [0, 1, 2, 2, 3, 0]
+    in makeMeshfromSpare "quad" verts ixs white
 
-mkTriMeshfromSpare :: String -> [Position4f] -> [Index] -> Color4f -> Mesh Vertex434
-mkTriMeshfromSpare id verts ixs color = traceShow' $ mkTriMesh id (processSpareVerts verts ixs color) (take (length ixs) [0..])
+makeMeshfromSpare :: String -> [Position4f] -> [Index] -> Color4f -> Mesh Vertex434
+makeMeshfromSpare id verts ixs color = traceShowS ("mesh: "++) $
+    mkTriMesh id (processSpareVerts verts ixs color) $ take (length ixs) [0..]
 
 
 -- | takes spare 3d-points (without duplicates) and the indices
 -- to construct the adequate attributes to be processed by opengl 
 processSpareVerts :: [Position4f] -> [Index] -> Color4f -> [Vertex434]
-processSpareVerts vs ixs color = genNormals $ extract vs ixs
+processSpareVerts vs' ixs color = 
+    let vs = traceShowS ("vs: " ++) $ extract vs' ixs
+        ns = (normals $ vs^..traverse._xyz)^..traverse.replicated 3 
+        cs = traceShowS ("cs: " ++) $ replicate (length vs) color
+    in zipWith3 Vertex vs ns cs
     where 
       extract :: [Position4f] -> [Index] -> [Position4f]
       extract vs = map (vs!!)
 
-      genNormals :: [Position4f] -> [Vertex434]
-      genNormals vs =
-        let ns = concat $ map (\(a:b:_) -> replicate 3 $ genNormal a b) $ splitEvery 3 vs
-            cs = repeat color
-        in zipWith3 Vertex vs ns cs
-
       -- | generate a normal with cross product from two vectors and flip the normal to
       -- show from the inside to the outside
-      genNormal :: Position4f -> Position4f -> Normal3f
-      genNormal v1 v2 = signorm $ (v1^._xyz) `cross` (v2^._xyz)
 
