41239fb788deb51d49132dae44476c24a38299f3
Jan-Philip Loos <maxdaten@gmail.com>
2014-06-04 04:16:35 +0200

    moved matrix-calculation to transformation

--- src/Yage/Rendering/Transformation.hs
+++ src/Yage/Rendering/Transformation.hs
@@ -21,3 +21,10 @@ makeLenses ''Transformation
 
 idTransformation :: (RealFloat a, Epsilon a) => Transformation a
 idTransformation = Transformation zero (axisAngle (V3 0 1 0) (deg2rad 0)) (V3 1 1 1)
+
+
+calcModelMatrix :: (Num a) => Transformation a -> M44 a
+calcModelMatrix trans =
+    let scaleM       = kronecker . point $ trans^.transScale
+        transM       = mkTransformation (trans^.transOrientation) (trans^.transPosition)
+    in transM !*! scaleM
