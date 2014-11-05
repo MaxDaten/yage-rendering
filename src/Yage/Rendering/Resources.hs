29f9425386ff5e9270c934220ef4343d77b6dcf3
Jan-Philip Loos <maxdaten@gmail.com>
2014-11-05 16:59:29 +0100

    docs an reexporting

--- src/Yage/Rendering/Resources.hs
+++ src/Yage/Rendering/Resources.hs
@@ -1,7 +1,8 @@
-module Yage.Rendering.Resources 
+module Yage.Rendering.Resources
     ( module Res
     ) where
 
 import Yage.Rendering.Resources.Manager         as Res
 import Yage.Rendering.Resources.Types           as Res
 import Yage.Rendering.Resources.ResTypes        as Res
+import Yage.Rendering.Textures.MipMapChain      as Res
--- src/Yage/Rendering/Textures/MipMapChain.hs
+++ src/Yage/Rendering/Textures/MipMapChain.hs
@@ -5,6 +5,10 @@ module Yage.Rendering.Textures.MipMapChain
 import           Prelude                             ( not, Bool, (.), null, ($) )
 import           Data.NonNull                        ( NonNull, nonNull, tail )
 
+-- | The first element in the 'MipMapChain' is the base element
+-- (e.g. 'Texture' or 'FilePath'). Every 'MipMapChain' has a base
+-- element (aka is never empty). The other elements in the chain are the mipmap
+-- levels in resoloution descending order.
 type MipMapChain tex = NonNull [tex]
 
 -- | creates a mipmap chain from a base and a mip map list
