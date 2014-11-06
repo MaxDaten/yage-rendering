89fed75cb653394dd6a333a9310338e0a66d20b2
Jan-Philip Loos <maxdaten@gmail.com>
2014-11-06 06:04:36 +0100

    added `MipMapChain` to reexport

--- src/Yage/Rendering/Textures.hs
+++ src/Yage/Rendering/Textures.hs
@@ -5,5 +5,6 @@ module Yage.Rendering.Textures (
 
 import Yage.Rendering.Textures.TextureCube  as Tex
 import Yage.Rendering.Textures.TextureImage as Tex
+import Yage.Rendering.Textures.MipMapChain  as Tex
 import Codec.Picture                        as Img
 import Codec.Picture.Types                  as Img
